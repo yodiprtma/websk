e6c7f15a626b5465886e9a7c78699729c947e9e9
yodiprtma <kdydprtma34@gmail.com>
2023-10-25 19:48:19 +0800

    TUGAS

--- app/Http/Controllers/MKController.php
+++ app/Http/Controllers/MKController.php
@@ -6,15 +6,45 @@ use Illuminate\Http\Request;
 
 class MKController extends Controller
 {
-    public function index()
-    {
-    }
+    private $data = [
+        [
+            'ID' => "MK123456",
+            'MK' => "Web Programing",
+            'jurusan' => "TI",
+        ],
+        [
+            'ID' => "MK123457",
+            'MK' => "OOP",
+            'jurusan' => "TI",
+        ],
+        [
+            'ID' => "MK123458",
+            'MK' => "Net Work",
+            'jurusan' => "TI",
+        ],
+        [
+            'ID' => "MK123459",
+            'MK' => "System C",
+            'jurusan' => "TI",
+        ],
+    ];
 
-    public function create()
-    {
+        public function index()
+        {
+            return view('mahasiswa.index', ['data' => $this->data]);
+        }
+    
+        public function create()
+        {
+            return view('mahasiswa.create');
+        }
+    
+        public function edit($id)
+        {
+            return view('mahasiswa.edit', ['data' => $this->data[$id], 'id' => $id]);
+        }
+    
+        public function show($id)
+        {
+        }
     }
-
-    public function show($id)
-    {
-    }
-}
